42b936e8d120ebf46695fe278c31724ed5bb5c4f
Miguel Lopez Rivera <miguellopezrivera@icloud.com>
2026-05-23 14:46:14 -0400

    Change URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-        site: 'https://leugimlra.github.io',
+        site: 'https://www.miguellr.dev',
 })
